661b39a9cfc00c3b7f71227103f5b0196d00b20b
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2025-02-19 12:14:07 +0300

    feat: Базовая разметка шаблона по умолчанию

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="container">
-    <WidgetsHeader />
+  <div class="default-template">
+    <WidgetsHeader/>
     <main>
-      <slot />
+      <slot/>
     </main>
     <footer class="footer">
       footer
     </footer>
   </div>
 </template>
+
+<style scoped>
+.default-template {
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  min-height: 100vh;
+}
+</style>
