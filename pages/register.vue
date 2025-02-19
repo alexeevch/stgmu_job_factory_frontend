a7a26b705dc37e5738d581ab76e2bb74ff382a6f
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2025-02-19 12:14:36 +0300

    feat: Обновлен компонент регистрации для проверки tailwind

--- pages/register.vue
+++ pages/register.vue
@@ -3,21 +3,30 @@ import { Form } from '@primevue/forms'
 </script>
 
 <template>
-  <div>
-    <Form v-slot="$form"
-          class="flex flex-col gap-4 w-full sm:w-100">
-      <div class="flex flex-col gap-1">
-        <InputText name="username" type="text" placeholder="Имя" fluid />
-        <Message v-if="$form.username?.invalid" severity="error" size="small" variant="simple">
-          {{ $form.username.error.message }}
-        </Message>
-      </div>
-      <ButtonGroup>
-        <Button label="Сохранить" icon="pi pi-check" severity="success"/>
-        <Button label="Удалить" icon="pi pi-trash" severity="danger"/>
-        <Button label="Отмена" icon="pi pi-times" severity="info"/>
-      </ButtonGroup>
-    </Form>
+  <div class="flex justify-center">
+    <Form :initialValues :resolver  class="flex flex-col gap-4 w-full sm:w-80">
+    <FormField v-slot="$field" name="username" initialValue="" class="flex flex-col gap-1">
+        <InputText type="text" placeholder="Логин" />
+        <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{ $field.error?.message }}</Message>
+    </FormField>
+    <FormField v-slot="$field" name="firstname" initialValue=""  class="flex flex-col gap-1">
+        <InputText type="text" placeholder="Имя" />
+        <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{ $field.error?.message }}</Message>
+    </FormField>
+    <FormField v-slot="$field" name="lastname" initialValue=""  class="flex flex-col gap-1">
+        <InputText type="text" placeholder="Фамилия" />
+        <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{ $field.error?.message }}</Message>
+    </FormField>
+    <FormField v-slot="$field" name="password" initialValue="" class="flex flex-col gap-1">
+        <Password type="text" placeholder="Пароль" :feedback="false" toggleMask fluid />
+        <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{ $field.error?.message }}</Message>
+    </FormField>
+    <FormField v-slot="$field" name="details" class="flex flex-col gap-1">
+        <Textarea placeholder="О себе" />
+        <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{ $field.error?.message }}</Message>
+    </FormField>
+    <Button type="submit" severity="secondary" label="Submit" />
+</Form>
   </div>
 </template>
 
