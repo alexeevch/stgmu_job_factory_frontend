30ea2ef98697093e68971dc422cc9455bb0a7741
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2025-02-20 15:13:52 +0300

    feat: Черновой вариант header

--- components/widgets/Header.vue
+++ components/widgets/Header.vue
@@ -1,51 +1,36 @@
-<script setup>
-import PersonalAccount from "~/components/buttons/personal-account.vue";
+<script lang="ts" setup>
+interface MenuItem {
+  name: string;
+  to: string;
+  label: string;
+}
+
+const menuItems: Array<MenuItem> = [
+  { name: "home", to: "/", label: "Главная" },
+  { name: "vacancies", to: "/vacancies", label: "Вакансии" },
+  { name: "resume", to: "/resume", label: "Разместить резюме" },
+  {
+    name: "public-vacancy",
+    to: "/public-vacancy",
+    label: "Опубликовать вакансию",
+  },
+];
 </script>
 
 <template>
-  <header class="header-container">
-    <PersonalAccount />
+  <header class="container mx-auto">
+    <ul
+      class="flex items-center justify-center shadow-sm w-full mt-8 bg-white dark:bg-gray-800 rounded-3xl align-middle"
+    >
+      <li
+        v-for="item in menuItems"
+        :key="item.name"
+        class="pl-7 pr-7 pb-4 pt-4 hover:text-link transition duration-300"
+      >
+        <router-link :to="item.to">
+          {{ item.label }}
+        </router-link>
+      </li>
+    </ul>
   </header>
 </template>
-
-<style scoped>
-/* Общий контейнер */
-.header-container {
-  margin-top: 1rem; /* Отступ от верха страницы */
-  display: flex;
-  justify-content: center; /* Центрирование меню по горизонтали */
-}
-
-/* Стили для "Dynamic Island" меню */
-.dynamic-island-menu {
-  display: flex;
-  gap: 0.5rem; /* Промежуток между кнопками */
-  background-color: #ffffff; /* Фоновое цвет меню */
-  border-radius: 24px; /* Скругление углов */
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Тень для эффекта отрыва */
-  padding: 0.5rem; /* Внутренний отступ */
-  overflow: hidden; /* Убираем лишние элементы за пределами контейнера */
-}
-
-/* Стили для элементов меню */
-.menu-item {
-  background-color: transparent; /* Прозрачный фон */
-  border: none; /* Без границ */
-  color: #333333; /* Цвет текста */
-  font-size: 1rem; /* Размер шрифта */
-  font-weight: 500; /* Полужирный шрифт */
-  padding: 0.75rem 1.5rem; /* Паддинг для удобства нажатия */
-  border-radius: 16px; /* Скругление кнопок */
-  transition:
-    background-color 0.3s ease,
-    color 0.3s ease; /* Анимация при наведении */
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-transform: capitalize; /* Каждое слово с большой буквы */
-}
-
-.menu-item.p-button-icon-only {
-  padding: 0.75rem; /* Для кнопок только с иконкой */
-}
-</style>
