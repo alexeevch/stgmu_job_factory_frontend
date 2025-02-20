c1a56bee52aac03fba507cac208ca650235f4c54
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2025-02-20 15:41:35 +0300

    feat: Динамическая смена META тегов по маршрутам

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+const config = useRuntimeConfig();
+const route = useRoute();
+
+useHead({
+  title: `${route.meta.title} - ${config.public.siteName}`,
+});
+</script>
+
 <template>
   <div>
     <NuxtLayout>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+definePageMeta({
+  title: "Главная страница",
+});
+</script>
 
 <template>
   <div>index page</div>
